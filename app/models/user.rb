5e007349ff625a5c787eeff4ff31cbcadbe9defe
Dewanshu Chhatlani <devanshu.chh@gmail.com>
2024-01-11 14:15:34 +0530

    Added validations for first_name and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
   has_many :tickets
 end
